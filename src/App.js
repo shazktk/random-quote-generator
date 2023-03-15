14b0ed878d05c7a67cc531371cf18ded4fb32e5f
Shahzeb Khan <skhan.bece15@nice.nust.edu.pk>
2023-03-15 19:27:03 +0500

    freecodecamp App tester
    Added freecodecamp app tester

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './Components/Button';
+import { useState, useEffect } from 'react';
 
 function App() {
+// const [quotesData, getQuotesData] = useState({
+//   quotes: [],
+//   selectedQuoteIndex: null
+// })
+  const [quotesData, getQuotesData] = useState([])
+
+ 
+useEffect(
+  fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+  .then(data=> data.json())
+  .then(data=> console.log(data))
+  .then(quotes=> getQuotesData(quotes))
+,[])
+
+
+  function nextQuoteClickHandler() 
+  {
+    console.log('Hi');
+  }
+  console.log(quotesData)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="quote-box">
+      {/* <Button buttonDisplayName="Next Quote" clickHandler={nextQuoteClickHandler}/> */}
+      <p>${quotesData}</p>
     </div>
   );
 }
